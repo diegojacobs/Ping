19b00d789c99ae426a38ea20cae580313fe91e32
diegojacobs <diegojacobs9595@gmail.com>
2017-03-20 15:28:15 -0600

    Bad Checksum

--- lab.pl
+++ lab.pl
@@ -1,30 +1,45 @@
 use Socket;
-use constant ICMP_ECHO_REQUEST => 8;
+use Sys::Hostname;
 
-socket(SOCKET, AF_INET, SOCK_RAW, 255) || die $!;
-setsockopt(SOCKET, 0, 1, 1);
+$dst_host = $ARGV[0];
 
-#ojo, las variables $srcHost, $srcPort, $dstHost, $dstPort no estan definidas, ustedes las tienen que leer de la linea de comandos
-my $packet = headers("192.168.0.34", 1, "173.194.215.113", 1);
-my $destination = pack('Sna4x8', AF_INET, 1, "173.194.215.113");
-$retVal = send(SOCKET,$packet,0,$destination);
-print $retVal;
+main();
 
-sub headers {
-  local($srcHost,$srcPort,$dstHost,$dstPort) = @_;
-  my $packet_id = int((rand(1000)));
-  #type 8, code 8, checksum 16, id 16, sequence 16
-  my $header = pack('ccnSs', ICMP_ECHO_REQUEST, 0,0, $id, 1);
-  #sending 69 bytes
-  my $data = "ICMP PROTOCOL HACK DATA ICMP PROTOCOL HACK DATA ICMP PROTOCOL HACK DATA";
-  #calcular checksum del header + la data
-  my $my_checksum = checksum($header.$data);
-  $header = pack('ccnSsLL', ICMP_ECHO_REQUEST, 0, 3342, $id, 1, "192.168.0.34", "1");
-  return $header.$data;
-  #aqui tienen que hacer su magia
+sub main { 
+    socket(SOCKET , AF_INET, SOCK_RAW, 1) or die $!;
+     
+    #set IP_HDRINCL to 1, this is necessary when the above protocol is something other than IPPROTO_RAW
+    my $srcHost = inet_ntoa((gethostbyname(hostname))[4]);
+    my $srcPort = 1;
+
+    my $dstHost = (gethostbyname($dst_host))[4];
+    my $dstPort = 1;
+
+    my $packet = headers($srcHost, $srcPort, $dstHost, $dstPort);
+     
+    my $destination = pack('Sna4x8', AF_INET, $dstPort, $dstHost);
+
+    my $bytesSent = send(SOCKET , $packet , 0 , $destination) or die $!;
+    print "PING " . $dst_host . " (" . $dst_host . ")" . ": " . $bytesSent . " bytes of data \n";
+    
 }
+ 
+sub headers {
+    local($src_host , $src_port , $dst_host , $dst_port) = @_;
+    
+    my $id = int(rand(1000));
+    
+    my $icmpHeader = pack('ccSSs', 8, 0,0, $id, 1);
+    my $data = "My Ping Test v1.0";
+    my $myChecksum = checksum($icmp_header . $data);
+    $icmpHeader = pack('ccnSs', 8, 0, ($myChecksum), $id, 1);
 
-#para el calculo del checksum podrian usar una funcion como la siguiente:
+    my $packet =  $icmpHeader . $data;
+     
+    return $packet;
+}
+ 
+#para el calculo del checksum podrian usar una funcion como la siguiente
 sub checksum {
     my $msg = shift;
     my $length = length($msg);
@@ -38,4 +53,4 @@ sub checksum {
     $sum += unpack("C", substr($msg, $length - 1, 1)) if $length % 2;
     $sum = ($sum >> 16) + ($sum & 0xffff);
     return(~(($sum >> 16) + $sum) & 0xffff);
-}
+} 
